1238eb3beced894c4b9fd84e3ed3740076d3ccf7
israelcvidal <israelcvidal@gmail.com>
2017-05-02 22:58:26 -0300

    adding interface to second phase of semantic analysis

--- src/devel/semantic_analysis/CheckTableVisitor.java
+++ src/devel/semantic_analysis/CheckTableVisitor.java
@@ -1,13 +1,44 @@
 package devel.semantic_analysis;
 
+import core.abstract_syntax.syntaxtree.And;
+import core.abstract_syntax.syntaxtree.ArrayAssign;
+import core.abstract_syntax.syntaxtree.ArrayLength;
+import core.abstract_syntax.syntaxtree.ArrayLookup;
+import core.abstract_syntax.syntaxtree.Assign;
+import core.abstract_syntax.syntaxtree.Block;
+import core.abstract_syntax.syntaxtree.BooleanType;
+import core.abstract_syntax.syntaxtree.Call;
 import core.abstract_syntax.syntaxtree.ClassDeclExtends;
 import core.abstract_syntax.syntaxtree.ClassDeclSimple;
+import core.abstract_syntax.syntaxtree.Exp;
+import core.abstract_syntax.syntaxtree.False;
 import core.abstract_syntax.syntaxtree.Formal;
+import core.abstract_syntax.syntaxtree.Identifier;
+import core.abstract_syntax.syntaxtree.IdentifierExp;
+import core.abstract_syntax.syntaxtree.IdentifierType;
+import core.abstract_syntax.syntaxtree.If;
+import core.abstract_syntax.syntaxtree.IntArrayType;
+import core.abstract_syntax.syntaxtree.IntegerLiteral;
+import core.abstract_syntax.syntaxtree.IntegerType;
+import core.abstract_syntax.syntaxtree.LessThan;
 import core.abstract_syntax.syntaxtree.MainClass;
 import core.abstract_syntax.syntaxtree.MethodDecl;
+import core.abstract_syntax.syntaxtree.Minus;
+import core.abstract_syntax.syntaxtree.NewArray;
+import core.abstract_syntax.syntaxtree.NewObject;
+import core.abstract_syntax.syntaxtree.Not;
+import core.abstract_syntax.syntaxtree.Plus;
+import core.abstract_syntax.syntaxtree.Print;
 import core.abstract_syntax.syntaxtree.Program;
+import core.abstract_syntax.syntaxtree.Statement;
+import core.abstract_syntax.syntaxtree.This;
+import core.abstract_syntax.syntaxtree.Times;
+import core.abstract_syntax.syntaxtree.True;
 import core.abstract_syntax.syntaxtree.Type;
 import core.abstract_syntax.syntaxtree.VarDecl;
+import core.abstract_syntax.syntaxtree.While;
+import core.abstract_syntax.visitor.TypeVisitor;
+import core.abstract_syntax.visitor.Visitor;
 
 /**
  * This class checks the table built by BuildTableVisitor in order to check if variables and methods used were declared and
@@ -15,48 +46,192 @@ import core.abstract_syntax.syntaxtree.VarDecl;
  * @author Israel
  */
 
-public class CheckTableVisitor implements TypeCheckVisitor {
-
+public class CheckTableVisitor implements SecondPhaseTypeCheckVisitor{
+	private ProgramTable programTable;
+	
+	public CheckTableVisitor(ProgramTable pt) {
+		this.programTable = pt;
+	}
+	
 	@Override
-	public ProgramTable visit(Program n) {
+	public Type visit(IntArrayType n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ClassTable visit(MainClass n) {
+	public Type visit(BooleanType n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ClassTable visit(ClassDeclSimple n) {
+	public Type visit(IntegerType n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ClassTable visit(ClassDeclExtends n) {
+	public Type visit(IdentifierType n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Type visit(VarDecl n) {
+	public Type visit(Block n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public MethodTable visit(MethodDecl n) {
+	public Type visit(If n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Type visit(Formal n) {
+	public Type visit(While n) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public Type visit(Print n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Assign n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(ArrayAssign n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(And n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(LessThan n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Plus n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Minus n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Times n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(ArrayLookup n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(ArrayLength n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Call n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(IntegerLiteral n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(True n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(False n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(IdentifierExp n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(This n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(NewArray n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(NewObject n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Not n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visit(Identifier n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ProgramTable getProgramTable() {
+		return programTable;
+	}
+
+	public void setProgramTable(ProgramTable programTable) {
+		this.programTable = programTable;
+	}
+
+
+
+	@Override
+	public Type visit(MainClass n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	  
+	
 
 }
---
+++ src/devel/semantic_analysis/SecondPhaseTypeCheckVisitor.java
@@ -0,0 +1,76 @@
+package devel.semantic_analysis;
+
+import core.abstract_syntax.syntaxtree.And;
+import core.abstract_syntax.syntaxtree.ArrayAssign;
+import core.abstract_syntax.syntaxtree.ArrayLength;
+import core.abstract_syntax.syntaxtree.ArrayLookup;
+import core.abstract_syntax.syntaxtree.Assign;
+import core.abstract_syntax.syntaxtree.Block;
+import core.abstract_syntax.syntaxtree.BooleanType;
+import core.abstract_syntax.syntaxtree.Call;
+import core.abstract_syntax.syntaxtree.ClassDeclExtends;
+import core.abstract_syntax.syntaxtree.ClassDeclSimple;
+import core.abstract_syntax.syntaxtree.False;
+import core.abstract_syntax.syntaxtree.Formal;
+import core.abstract_syntax.syntaxtree.Identifier;
+import core.abstract_syntax.syntaxtree.IdentifierExp;
+import core.abstract_syntax.syntaxtree.IdentifierType;
+import core.abstract_syntax.syntaxtree.If;
+import core.abstract_syntax.syntaxtree.IntArrayType;
+import core.abstract_syntax.syntaxtree.IntegerLiteral;
+import core.abstract_syntax.syntaxtree.IntegerType;
+import core.abstract_syntax.syntaxtree.LessThan;
+import core.abstract_syntax.syntaxtree.MainClass;
+import core.abstract_syntax.syntaxtree.MethodDecl;
+import core.abstract_syntax.syntaxtree.Minus;
+import core.abstract_syntax.syntaxtree.NewArray;
+import core.abstract_syntax.syntaxtree.NewObject;
+import core.abstract_syntax.syntaxtree.Not;
+import core.abstract_syntax.syntaxtree.Plus;
+import core.abstract_syntax.syntaxtree.Print;
+import core.abstract_syntax.syntaxtree.Program;
+import core.abstract_syntax.syntaxtree.This;
+import core.abstract_syntax.syntaxtree.Times;
+import core.abstract_syntax.syntaxtree.True;
+import core.abstract_syntax.syntaxtree.Type;
+import core.abstract_syntax.syntaxtree.VarDecl;
+import core.abstract_syntax.syntaxtree.While;
+
+public interface SecondPhaseTypeCheckVisitor {
+//	public Type visit(Program n);
+	public Type visit(MainClass n);
+//	public Type visit(ClassDeclSimple n);
+//	public Type visit(ClassDeclExtends n);
+//	public Type visit(VarDecl n);
+//	public Type visit(MethodDecl n);
+//	public Type visit(Formal n);
+	public Type visit(IntArrayType n);
+	public Type visit(BooleanType n);
+	public Type visit(IntegerType n);
+	public Type visit(IdentifierType n);
+	public Type visit(Block n);
+	public Type visit(If n);
+	public Type visit(While n);
+	public Type visit(Print n);
+	public Type visit(Assign n);
+	public Type visit(ArrayAssign n);
+	public Type visit(And n);
+	public Type visit(LessThan n);
+	public Type visit(Plus n);
+	public Type visit(Minus n);
+	public Type visit(Times n);
+	public Type visit(ArrayLookup n);
+	public Type visit(ArrayLength n);
+	public Type visit(IntegerLiteral n);
+	public Type visit(Call n);
+	public Type visit(True n);
+	public Type visit(False n);
+	public Type visit(IdentifierExp n);
+	public Type visit(This n);
+	public Type visit(NewArray n);
+	public Type visit(NewObject n);
+	public Type visit(Not n);
+public Type visit(Identifier n);
+
+
+}
