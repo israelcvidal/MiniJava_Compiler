483385b20a4c250b4ad40321587066d92629b686
Lucas Peres Gaspar <Lucas Peres Gaspar>
2017-05-27 09:24:57 -0300

    Minnor fix

--- src/core/translation_to_IR/tree/AbstractExp.java
+++ src/core/translation_to_IR/tree/AbstractExp.java
@@ -6,5 +6,4 @@ abstract public class AbstractExp {
 	abstract public ExpList kids();
 	abstract public AbstractExp build(ExpList kids);
 	
-	abstract public void accpet(Visitor v);
 }
--- src/core/translation_to_IR/tree/Stm.java
+++ src/core/translation_to_IR/tree/Stm.java
@@ -5,7 +5,5 @@ import core.translation_to_IR.visitor.Visitor;
 abstract public class Stm {
 	abstract public ExpList kids();
 	abstract public Stm build(ExpList kids);
-	
-	abstract public void accpet(Visitor v);
 }
 
