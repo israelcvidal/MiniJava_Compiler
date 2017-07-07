e6e50d56c249a402858843d0acfe4132e2bbb6e0
Israel Vidal <israelcvidal@gmail.com>
2017-07-07 15:07:18 -0300

    Generating Flow Graph

--- src/devel/assemble_flow_graph/AssemFlowGraph.java
+++ src/devel/assemble_flow_graph/AssemFlowGraph.java
@@ -1,5 +1,8 @@
 package devel.assemble_flow_graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import core.activation_records.temp.Label;
 import core.activation_records.temp.LabelList;
 import core.activation_records.temp.TempList;
@@ -14,28 +17,58 @@ public class AssemFlowGraph extends FlowGraph{
 	
 	public Instr instr(Node n){
 		
-		return null;
+		return n.getInstruction();
 	}
 
 	public AssemFlowGraph(InstrList instrs){
-		
+		HashMap<Label, Node> labelMap = new HashMap<>();
+
 		for(InstrList p=instrs; p!=null; p=p.tail){
-		
-			Instr instr_a = instrs.head;
-			Node node_a = flowGraph.newNode(instr_a);
 			
+			Instr instr = instrs.head;
+			Node node = flowGraph.newNode(instr);
+						
+//			if the instruction is an label, add the map label=>node to the hashMap
+			if(instr.getClass() == LABEL.class)
+				labelMap.put(((LABEL)instr).label, node);
+		}
+		
+//		Now we hash the map of all label instructions to its nodes. The we add the 
+		for(InstrList p=instrs; p!=null; p=p.tail){
+			
+			Instr instr_a = instrs.head;
+			Instr instr_b = null;
+			Node node_a = null;
+			Node node_b = null;
+
+			if(instrs.tail!= null)
+				instr_b = instrs.tail.head;
+				
+//			Getting node from instr_a
+			for(NodeList nodes = flowGraph.nodes(); nodes!= null && node_a == null; nodes=nodes.tail){
+				if(instr(nodes.head) == instr_a)
+					node_a = nodes.head;
+			}
+			
+//			Getting node from instr_b
+			for(NodeList nodes = flowGraph.nodes(); nodes!= null && node_b == null; nodes=nodes.tail){
+				if(instr(nodes.head) == instr_b)
+					node_b = nodes.head;
+			}
+			
+//			Adding edge to next instruction in list
+			if(node_b != null)
+				flowGraph.addEdge(node_a, node_b);
+			
+			node_b = null;
+//			Adding edge to the label instr_a jumps to 
 			for( LabelList targets = instr_a.jumps().labels; targets!=null; targets = targets.tail){
 				Label label_b = targets.head;
-//				Node node_b = flowGraph.newNode(label_b);
-				
-				flowGraph.addEdge(node_a, node_b);
-				
-				
-				
-				
+				node_b = labelMap.get(label_b);
+				if(node_b != null)
+					flowGraph.addEdge(node_a, node_b);
 			}
-					
-		}
+		}	
 	}
 	
 	@Override
